9340462f0b80616a3ebf7ae823540ec3df1a97b3
VaLa-Laiton <angeldavidvl2005@gmail.com>
2024-09-29 20:47:48 -0500

    hello.py Completed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # helloPython
-"Repository dedicated to documenting my progress and learning process in Python.
+Repository dedicated to documenting my progress and learning process in Python.
+
+## hello.py
+Python, like other languages, has multiple data types. These include integers, floats, strings, among others. This is to perform precise and efficient operations.
---
+++ hello.py
@@ -0,0 +1,28 @@
+# Single Line Comment: Hello Python
+print('Hello Python 👋')
+
+""" 
+Multiline Comments:
+Math Operations
+"""
+print('--- Math Operations ---')
+
+print('Addition: 3+2 =', 3+2)
+print('Subtraction: 3-2 =', 3-2)
+print('Multiplication: 3*2 =', 3*2)
+print('Division: 3/2 =', 3/2)
+print('Exponential: 3**2 =', 3**2)
+print('Modulus: 3%2 =', 3%2)
+print('Floor division operator: 3//2 =', 3//2)
+
+print('--- Data Types ---')
+
+print('Integers: 10', type(10))                  
+print('Decimal Numbers: 3.14',type(3.14))                
+print('Complex Numbers: 1 + 3j',type(1 + 3j))              
+print('Strings: "Angel David"',type('Angel David'))          
+print('Lists: [1, 2, 3]',type([1, 2, 3]))           
+print('Dictionaries: {"name":"Angel David"}',type({'name':'Angel David'})) 
+print('Sets: {9.8, 3.14, 2.7}',type({9.8, 3.14, 2.7}))    
+print('Tuples: (9.8, 3.14, 2.7)',type((9.8, 3.14, 2.7)))  
+print('Booleans: True', type(True))
